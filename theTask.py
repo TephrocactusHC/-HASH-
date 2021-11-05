95485a839d10404d7d65f1bbeca0110a0b2fda9b
TephrocactusHC <93771873+TephrocactusHC@users.noreply.github.com>
2021-11-05 20:08:57 +0800

    将结果放在csv文件中
    将结果放在csv文件中

---
+++ theTask.py
@@ -0,0 +1,67 @@
+import os
+import hashlib
+import functools
+import csv
+
+#输出文件的存储路径
+def savepath():
+    global savepath
+    savepath=input('please input the target path:')
+    return savepath
+
+#判断子文件夹
+def judge(path):
+    if os.path.isdir(path)==True:
+        list_files = os.listdir(path)
+        return list_files
+
+# 合并
+def add(x, y):
+    return x + y
+
+#合成文件路径
+def addpath(file):
+    path = dir_root+'\\'+file
+    return path
+
+#获得文件的绝对路径
+def getpath(path):
+    os.chdir(path)
+    global dir_root
+    dir_root=os.getcwd()
+    list_files=os.listdir(path)
+    list_files=list(map(addpath,list_files))
+    return list_files
+
+def getfile(path):
+    os.chdir(path)
+    list_items = list( map(os.path.abspath,list(os.listdir(path))))
+    list_files = list(filter(os.path.isfile, list_items))
+    if list_files!=None:
+        list(map(compute_hash,list_files))
+    list_dirs = list(filter(os.path.isdir, list_items))
+    if list_dirs!=None:
+        if list(map(judge,list_dirs))!=None:
+            list(map(getfile,list_dirs))
+        else:
+            list_dirs = list(map(getpath, list_dirs))
+            list_dirs = list(functools.reduce(add,list_dirs))
+            list(map(compute_hash,list_dirs))
+
+# 计算文件的md5和sha256值并写入csv文件
+def compute_hash(file):
+    with open(file,'rb') as f:
+        data = f.read()
+        filepath=os.path.abspath(file)
+        file_md5 = hashlib.md5(data).hexdigest()
+        file_sha256 = hashlib.sha256(data).hexdigest()
+    outputFile = open(savepath, 'a', newline='')
+    outputWriter = csv.writer(outputFile)
+    outputWriter.writerow([filepath, file_md5,file_sha256,])
+    outputFile.close()
+
+if __name__=='__main__':
+    thepath=input('input a path:')
+    savepath()
+    getfile(thepath)
+
